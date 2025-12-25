94fc8d31be2b577bfefd2f8858c468f0eb5878e3
haoke <haoke@haokedeMac-mini.local>
2025-12-25 14:37:56 +0800

    fix

--- frontend/src/pages/campaigns/index.tsx
+++ frontend/src/pages/campaigns/index.tsx
@@ -418,16 +418,16 @@ export default function CampaignsPage() {
         // 加载中显示骨架屏
         <Card>
           <div className="overflow-x-auto">
-            <Table className="min-w-[900px]">
+            <Table className="min-w-[900px] table-fixed">
               <TableHeader>
               <TableRow>
                 <TableHead className="w-[60px]">ID</TableHead>
-                <TableHead className="w-[180px]">标题</TableHead>
+                <TableHead>标题</TableHead>
                 <TableHead className="text-center w-20">状态</TableHead>
                 <TableHead>目标列表</TableHead>
-                <TableHead>发送进度</TableHead>
-                <TableHead className="w-[180px]">预定发送时间</TableHead>
-                <TableHead className="text-right w-[140px]">操作</TableHead>
+                <TableHead className="w-[160px]">发送进度</TableHead>
+                <TableHead className="w-[140px]">预定发送时间</TableHead>
+                <TableHead className="text-right w-[200px]">操作</TableHead>
               </TableRow>
             </TableHeader>
             <TableBody>
@@ -475,18 +475,18 @@ export default function CampaignsPage() {
       ) : (
         <Card>
           <div className="overflow-x-auto">
-            <Table className="min-w-[900px]">
+            <Table className="min-w-[900px] table-fixed">
               <TableHeader>
               <TableRow>
                 <TableHead className="w-[60px]">ID</TableHead>
-                <TableHead className="w-[180px]">标题</TableHead>
+                <TableHead>标题</TableHead>
                 <TableHead className="text-center w-20">状态</TableHead>
-                <TableHead className="w-[150px]">列表</TableHead>
+                <TableHead>列表</TableHead>
                 <TableHead className="text-center w-[160px]">发送进度</TableHead>
                 <TableHead className="text-center w-[100px]">打开率</TableHead>
                 <TableHead className="text-center w-[100px]">点击率</TableHead>
                 <TableHead className="text-center w-[140px]">时间</TableHead>
-                <TableHead className="text-right">操作</TableHead>
+                <TableHead className="text-right w-[200px]">操作</TableHead>
               </TableRow>
             </TableHeader>
             <TableBody>
